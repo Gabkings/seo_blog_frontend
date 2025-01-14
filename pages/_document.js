3dc91dad50e7f7eac0242dcecd227e79219ec338
Gabriel Gitonga <gabworks51@gmail.com>
2025-01-14 19:24:07 +0300

    Adding blog creation feature

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,10 @@ class MyDocument extends Document {
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
                   integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
                   crossOrigin="anonymous" referrerpolicy="no-referrer"/>
+            <link href="
+https://cdn.jsdelivr.net/npm/react-quill@2.0.0/dist/quill.snow.min.css
+" rel="stylesheet" />
+            <link rel="stylesheet" href="..." referrerPolicy="no-referrer" />
         </Head>
         <body>
           <Main />
